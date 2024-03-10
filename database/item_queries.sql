4b72326ffcbca97cb09300e2606809e71f1a2c20
Jesus <40635808+jgarciagalvez@users.noreply.github.com>
2024-03-10 16:16:54 +0700

    Adds a row to items table

---
+++ database/item_queries.sql
@@ -0,0 +1,15 @@
+-- Query to insert 1 row in the 'items' table
+
+INSERT INTO items (
+    user_id, 
+    parent_id, 
+    emoji, 
+    name, 
+    description
+    ) 
+VALUES (
+    1, 
+    NULL, 
+    '📝', 
+    'Project Alpha', 
+    'Initial phase of Project Alpha');
